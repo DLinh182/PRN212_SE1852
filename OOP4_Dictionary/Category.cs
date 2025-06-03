ece8ab6b4588ecb48ef88155e33ac1f57f9bd4f4
DLinh182 <dieulinh180204@gmail.com>
2025-06-03 07:10:01 +0700

    week 3

---
+++ OOP4_Dictionary/Category.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4_Dictionary
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Dictionary<int, Product> Products { get; set; }
+        public Category()
+        {
+            Products = new Dictionary<int, Product>();
+        }
+        //CRUD
+
+        //them moi sanphamcho Category
+        public void AddProduct(Product p)
+        {
+            if (Products.ContainsKey(p.Id) == false)
+            {
+                Products.Add(p.Id, p);
+            }
+        }
+        public void PrintAllProduct()
+        {
+            foreach (KeyValuePair<int, Product> item in Products)
+            {
+                Product p = item.Value;
+                Console.WriteLine(p);
+            }
+        }
+
+        //lấy sản phầm theo id 
+        public Product GetProduct(int id)
+        {
+            if (Products.ContainsKey(id) == false)
+                return null;
+            return Products[id];
+        }
+
+        public Dictionary<int, Product>SortProduct() 
+        { 
+            return Products.OrderBy(item => item.Value.Price).ToDictionary<int, Product>();
+        }
+
+        //sắp xếp giá tăng dần, nếu giá trùng nhau thì quantity giảm dần
+        //phương thức sau khi đè lên phương thức trước
+        public Dictionary<int, Product>ComplexSort()
+        {
+            return Products
+                .OrderByDescending(item => item.Value.Quantity)
+                .OrderBy(item => item.Value.Price)
+                .ToDictionary<int, Product>();
+        }
+
+
+        //sửa
+        public void EditProduct(Product p)
+        {
+            if(Products.ContainsKey(p.Id) == false)
+            {
+                return;
+            }
+            //Sửa dữ liệu tại ô nhớ có chứa p.id 
+            Products[p.Id] = p;
+        }
+
+        //xóa
+        public bool RemoveProduct(int id)
+        {
+            if (Products.ContainsKey(Id) == false)
+            {
+                return false;
+            }
+            //Sửa dữ liệu tại ô nhớ có chứa p.id 
+            return Products.Remove(Id);
+        }
+
+
+        //xóa tất cả sản phẩm bán ế trong quý 1 2025
+        //ế : chỉ bán được 1 sản phẩm 
+
+
+    }
+}
+
---
+++ OOP4_Dictionary/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using OOP4_Dictionary;
+Console.OutputEncoding=Encoding.UTF8;
+
+Category laptop = new Category();
+laptop.Id = 1;
+laptop.Name = "Danh muc Laptop";
+
+Product dell1 = new Product()
+{ Id = 1, Name = "Dell 1", Quantity = 10, Price = 15000000 };
+laptop.AddProduct(dell1);
+
+Product dell2 = new Product()
+{ Id = 2, Name = "Dell 2", Quantity = 20, Price = 20000000 };
+laptop.AddProduct(dell2);
+
+Product dell3 = new Product()
+{ Id = 3, Name = "Dell 3", Quantity = 15, Price = 17000000 };
+laptop.AddProduct(dell3);
+
+Product HP2 = new Product()
+{ Id = 4, Name = "HP2", Quantity = 20, Price = 12000000 };
+laptop.AddProduct(HP2);
+
+Product HP3 = new Product()
+{ Id = 5, Name = "HP3", Quantity = 15, Price = 12000000 };
+laptop.AddProduct(HP3);
+
+Console.WriteLine("Danh sach san pham cua danh muc Laptop");
+laptop.PrintAllProduct();
+
+Console.WriteLine("Nhap id san pham can tim:");
+int pid = int.Parse(Console.ReadLine());
+Product p = laptop.GetProduct(pid);
+if (p != null)
+{
+    Console.WriteLine("San pham co id = " + pid + " la:");
+    Console.WriteLine(p);
+}
+else
+{
+    Console.WriteLine("Khong tim thay san pham co id = " + pid);
+}
+
+Console.WriteLine("Danh sách");
+laptop.PrintAllProduct();
+Dictionary<int, Product> sortedproducts = laptop.SortProduct();
+Console.WriteLine("Sau sắp xếp: ");
+foreach(KeyValuePair<int, Product> item in sortedproducts)
+{
+    Product x = item.Value;
+    Console.WriteLine(x);
+}
+
+Dictionary<int, Product> complexSort = laptop.ComplexSort();
+Console.WriteLine("Sau sắp xếp phức tạp : ");
+foreach (KeyValuePair<int, Product> item in complexSort)
+{
+    Product x = item.Value;
+    Console.WriteLine(x);
+}
+
+
+Product px = new Product();
+px.Id = 1;
+px.Name = "macbook";
+px.Quantity = 122;
+px.Price = 3456789;
+laptop.EditProduct(px);
+Console.WriteLine("danh sách sau khi sửa");
+laptop.PrintAllProduct();
+
+int pid_remove = 1;
+laptop.RemoveProduct(pid_remove);
+Console.WriteLine("danh sách sau khi sửa");
+laptop.PrintAllProduct();
