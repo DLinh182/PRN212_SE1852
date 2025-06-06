f6ac47962a01a05fa8ba2770f0a658f8030eb450
DLinh182 <dieulinh180204@gmail.com>
2025-06-06 09:15:10 +0700

    w4 slot 2

--- DemoMyStoreLINQ2SQL/Program.cs
+++ DemoMyStoreLINQ2SQL/Program.cs
@@ -1,2 +1,83 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text;
+using DemoMyStoreLINQ2SQL;
+
+Console.OutputEncoding=Encoding.UTF8;
+
+string connectionString = "server=localhost\\SQLEXPRESS;database=MyStore;uid=sa;pwd=12345";
+MyStoreDataContext context = new MyStoreDataContext(connectionString);
+//Caau 1: dungf linq2sql ddeer laays toanf booj danh mucj
+var categories = context.Categories;
+foreach (var cate in categories)
+{
+    Console.WriteLine(cate.CategoryID+"\t"+cate.CategoryName);
+}
+
+//Caau 2: timf chi tieets 1 danh mujc khi bieets id
+int catid = 5;
+Category category = context.Categories.FirstOrDefault(c => c.CategoryID == catid);
+if(category == null)
+{
+    Console.WriteLine("Khong tim thay danh muc voi id = " + catid);
+}
+else
+{
+    Console.WriteLine($"danh muc co id = {catid}");
+    Console.WriteLine($"Ten danh muc: {category.CategoryName}");
+}
+
+//caau 3 : theem mowsi 1 danh muc
+/*Category cnew = new Category();
+cnew.CategoryName = "thuoc";
+context.Categories.InsertOnSubmit(cnew);
+context.SubmitChanges();*/
+
+//caau 4: theem mosiw nhieeuf danh muc cung 1 luc
+/*List<Category> dsdm = new List<Category>();
+dsdm.Add(new Category() { CategoryName = "dau an" });
+dsdm.Add(new Category() { CategoryName = "dau goi" });
+dsdm.Add(new Category() { CategoryName = "dau xa" });
+context.Categories.InsertAllOnSubmit(dsdm);
+context.SubmitChanges();
+*/
+
+//caau 5: sua ten danh muc
+//nguyen tac : tim thay moi sua
+Category c = (from x in context.Categories
+            where x.CategoryID == 5
+            select x).FirstOrDefault();
+
+Category c2 = context.Categories.FirstOrDefault(x => x.CategoryID == 10); 
+if(c2 != null)
+{
+    c2.CategoryName = "sua lai r";
+    context.SubmitChanges();
+}
+
+//Cau 6 : xoa danh muc
+//tim thay danh muc r moi xoa
+Category c3 = context.Categories.FirstOrDefault(x => x.CategoryID == 12);
+if (c3 != null)
+{
+    context.Categories.DeleteOnSubmit(c3);
+    context.SubmitChanges();
+}
+else
+{
+    Console.WriteLine("Khong tim thay danh muc can xoa");
+}
+
+//cau 7: xoa tat ca danh muc ko có san pham
+var listdel = from x in context.Categories
+              where !x.Products.Any()
+              select x;
+if(listdel.Any())
+{
+    context.Categories.DeleteAllOnSubmit(listdel);
+    context.SubmitChanges();
+}
+else
+{
+    Console.WriteLine("Khong co danh muc nao khong co san pham");
+}
+
---
+++ LucyDemoLINQ2SQL/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using LucyDemoLINQ2SQL;
+Console.OutputEncoding= Encoding.UTF8;
+
+//câu 1 : xuất danh sách khách hàng đã mua hàng
+string connectionString = "server=localhost\\SQLEXPRESS;database=Lucy_SalesData;uid=sa;pwd=12345";
+Lucy_SalesDataDataContext context = new Lucy_SalesDataDataContext(connectionString);
+var ds1 = from c in context.Customers
+          where c.Orders.Count() > 0
+          select c;
+Console.WriteLine("Danh sách khách hàng đã mua hàng:");
+foreach (var c in ds1)
+{
+    Console.WriteLine($"{c.CustomerID}\t{c.CompanyName}\t{c.ContactName}");
+}
+
+//Cau 2 : lấy 3 khách hàng có tổng giá trị mua hàng nhiều nhất
+var ds2 = (from c in context.Customers
+           join o in context.Orders on c.CustomerID equals o.CustomerID
+           join od in context.Order_Details on o.OrderID equals od.OrderID
+           group od by new { c.CustomerID, c.ContactName } into g
+           select new
+           {
+               CustomerID = g.Key.CustomerID,
+               ContactName = g.Key.ContactName,
+               TotalAmount = g.Sum(x => x.UnitPrice * x.Quantity)
+           }).OrderByDescending(x => x.TotalAmount)
+             .Take(3);
+
+foreach (var item in ds2)
+{
+    Console.WriteLine(item.CustomerID + "\t" + item.ContactName + "\t" + item.TotalAmount);
+}
+
