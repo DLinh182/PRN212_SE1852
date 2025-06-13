dd4c81cf0bd7dbd9bae2be0836ee4400410855b5
DLinh182 <dieulinh180204@gmail.com>
2025-06-13 08:42:04 +0700

    ButtonControl_TongHop

---
+++ ButtonControl_TongHop/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+
+namespace ButtonControl_TongHop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            // Thoát chương trình
+            Close();
+        }
+
+        private void BtnTong_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(txtA.Text);
+                int b = int.Parse(txtB.Text);
+                int tong = a + b;
+                tbKetQua.Text = tong.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Vui lòng nhập đúng định dạng số nguyên!", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
