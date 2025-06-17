3bf154f474e479bc434793068fcc9f1600e40a3e
DLinh182 <dieulinh180204@gmail.com>
2025-06-17 09:09:51 +0700

    list control + wpf

---
+++ ListWPF/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace ListWPF
+{
+    public partial class MainWindow : Window
+    {
+        List<int> dsDuLieu = new List<int>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnThem_Click(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(txtGiaTri.Text);
+            dsDuLieu.Add(x);
+            HienThiDanhDanh();
+            txtGiaTri.Text = "";
+        }
+
+        // Hàm hiển thị danh sách lên giao diện
+        void HienThiDanhDanh()
+        {
+            lstDuLieu.Items.Clear();
+            for (int i = 0; i < dsDuLieu.Count; i++)
+            {
+                int x = dsDuLieu[i];
+                lstDuLieu.Items.Add(x);
+            }
+        }
+
+        private void BtnChen_Click(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(txtGiaTriChen.Text);
+            int vt = int.Parse(txtViTriChen.Text);
+            dsDuLieu.Insert(vt, x);
+            HienThiDanhDanh();
+            txtViTriChen.Text = "";
+            txtGiaTriChen.Text = "";
+        }
+
+        private void BtnSapXepTang_Click(object sender, RoutedEventArgs e)
+        {
+            dsDuLieu.Sort();
+            HienThiDanhDanh();
+        }
+
+        private void BtnSapXepGiam_Click(object sender, RoutedEventArgs e)
+        {
+            dsDuLieu.Sort();
+            dsDuLieu.Reverse();
+            HienThiDanhDanh();
+        }
+
+        private void BtnXoa1PhanTu_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstDuLieu.SelectedIndex == -1)
+            {
+                MessageBox.Show("Phải chọn phần tử mới xóa được", "Thông báo lỗi", MessageBoxButton.OK);
+                return;
+            }
+            dsDuLieu.RemoveAt(lstDuLieu.SelectedIndex);
+            HienThiDanhDanh();
+        }
+
+        private void BtnXoaNhieuPhanTu_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstDuLieu.SelectedIndex == -1)
+            {
+                MessageBox.Show("Phải chọn phần tử mới xóa được", "Thông báo lỗi", MessageBoxButton.OK);
+                return;
+            }
+            // vòng lặp lấy các phần tử được chọn trên giao diện
+            while (lstDuLieu.SelectedItems.Count > 0)
+            {
+                // lấy phần tử đầu tiên ra
+                int data = (int)lstDuLieu.SelectedItems[0];
+                // xóa khỏi danh sách
+                dsDuLieu.Remove(data);
+                lstDuLieu.Items.Remove(data);
+            }
+        }
+        
+        private void BtnXoaToanBoPhanTu_Click(object sender, RoutedEventArgs e)
+        {
+            dsDuLieu.Clear();
+            HienThiDanhDanh();
+        }
+    }
+}
